3626bfde87da6358fae54088621fed341cc46264
Errol <errol@Errols-MacBook-Pro.local>
2023-09-24 08:02:08 -0600

    removed all debugs and local uploads

---
+++ gcs.py
@@ -0,0 +1,153 @@
+from gcloud import storage
+from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2 import service_account
+import os
+import json
+import base64
+import binascii
+from datetime import timedelta
+
+DEBUG = True
+LOCAL_DEV = True
+
+
+class GCS:
+    def __init__(self, project, bucket):
+        self.project = project
+        self.bucket = bucket
+        if LOCAL_DEV:
+            KEYFILE = '/Users/errol/gcpKeys/tstreck.json'
+            if os.path.exists(KEYFILE):
+                with open(KEYFILE) as jsonFile:
+                    self.credentials_dict = json.load(jsonFile)
+            else:
+                print("do something")
+                exit(199)
+            # print(credentials_dict)
+            self.credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+                self.credentials_dict
+            )
+            # self.client = storage.Client(credentials=self.credentials,
+            #  project=self.project)
+        else:
+            self.client = storage.Client()
+        # self.storageBucket = self.client.get_bucket(self.bucket)
+
+    def uploadFile(self, file, filename, uploader, email):
+        client = storage.Client(credentials=self.credentials,
+                                project=self.project)
+        storageBucket = client.get_bucket(self.bucket)
+        blob = self.storageBucket.blob(filename)
+        blob.content_type = file.content_type
+
+        try:
+
+            blob.upload_from_file(file)
+            {'uploader': {uploader}, 'email': {email}}
+            metadata = {'uploader': uploader, 'email': email}
+            blob.metadata = metadata
+            blob.patch()
+
+        except Exception as e:
+
+            print(e)
+            return "Upload Failed"
+        return "Upload Succeeded"
+
+    def generate_upload_signed_url_v4(self, blob_name):
+        """Generates a v4 signed URL for uploading a blob using HTTP PUT.
+
+        Note that this method requires a service account key file. You can not use
+        this if you are using Application Default Credentials from Google Compute
+        Engine or from the Google Cloud SDK.
+        """
+        # bucket_name = 'your-bucket-name'
+        # blob_name = 'your-object-name'
+        storage_client = storage.Client(credentials=self.credentials)
+        bucket = storage_client.bucket(self.bucket)
+        blob = bucket.blob(blob_name)
+
+        url = blob.generate_signed_url(
+            version="v4",
+            # This URL is valid for 15 minutes
+            expiration=timedelta(minutes=15),
+            # Allow PUT requests using this URL.
+            method="PUT",
+            content_type="application/octet-stream",
+        )
+
+        # print("Generated PUT signed URL:")
+        # print(url)
+        # print("You can use this URL with any user agent, for example:")
+        # print(
+        #     "curl -X PUT -H 'Content-Type: application/octet-stream' "
+        #     "--upload-file my-file '{}'".format(url)
+        # )
+        print(url)
+        return url
+
+    def fileExists(self, filename):
+
+        storage_client = storage.Client(credentials=self.credentials)
+        bucket = storage_client.bucket(self.bucket)
+        blob = bucket.blob(filename)
+
+        return blob.exists()
+
+    def getFiles(self, prefix=None):
+
+        storage_client = storage.Client(credentials=self.credentials)
+        bucket = storage_client.bucket(self.bucket)
+
+        return bucket.list_blobs(prefix=prefix)
+        # for file in files:
+        #     print("")
+        # return files
+
+    def listFiles(self, prefix=None):
+
+        storage_client = storage.Client(credentials=self.credentials)
+        bucket = storage_client.bucket(self.bucket)
+
+        fileList = []
+        for file in bucket.list_blobs(prefix=prefix):
+            fileList.append(file.name)
+        return fileList
+        # for file in files:
+        #     print("")
+        # return files
+
+    def dupExists(self, inputHash=None, prefix=None):
+
+        storage_client = storage.Client(credentials=self.credentials)
+        bucket = storage_client.bucket(self.bucket)
+
+        for i in self.listFiles(prefix):
+            blob = bucket.get_blob(i)
+            hash = binascii.hexlify(base64.urlsafe_b64decode(blob.md5_hash))
+            fileHash = bytes.decode(hash, 'utf-8')
+            # print(f'Inp: {inputHash}\nCur: {fileHash}')
+            if inputHash == fileHash:
+                return True
+        return False
+
+    def getImage(self, image):
+        blob = self.storageBucket.get_blob(image)
+        print(f'getImage: {blob} - {image}')
+        content = blob.download_as_string()
+        imageData = base64.b64encode(content).decode("utf-8")
+        # print(blob.content_type)
+        # print(type(content))
+        # print(dir(imageData))
+        return [imageData, blob.content_type, blob.metadata]
+
+
+# gcs = GCS(project='theresastrecker', bucket='theresa-photo-storage')
+# print(gcs.fileExists("uploads/dumpsterfire.jpg"))
+# # print(fileExists("testFolder/dumpsterfsdfire.jpg"))
+# print(gcs.listFiles())
+if __name__ == "__main__":
+    # Used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    gcs = GCS('theresastrecker', 'errol-test-bucket')
--- main_test.py
+++ main_test.py
@@ -1,27 +1,54 @@
 import hashlib
 from flask import Flask, render_template, request
+from gcloud import storage
+from google.oauth2 import service_account
 from werkzeug.utils import secure_filename
 from werkzeug.exceptions import RequestEntityTooLarge
 import os
 import gcs
+import requests
+import
+from datetime import timedelta
 
 
 app = Flask(__name__)
 
 ## Globals ##
-app.config['MAX_CONTENT_LENGTH'] = 784 * 1024 * 1024  # 784 MB
+app.config['MAX_CONTENT_LENGTH'] = 32000 * 1024 * 1024  # 32 MB
 app.config['UPLOAD_FOLDER'] = "uploads/"
 app.config['GCS_UPLOAD'] = True
+app.config['LOCAL_UPLOAD'] = False
+# credentials = service_account.Credentials.from_service_account_file(
+#     '/Users/errol/gcpKeys/tstreck.json')
+# client = storage.Client(credentials=credentials, project='theresastrecker')
+
+
+DEBUG = True
+bucketName = 'errol-test-bucket'
+
 cloudStorage = gcs.GCS(project='theresastrecker',
-                       bucket='theresa-photo-storage')
+                       bucket=bucketName)
+
+EXPIRATION = timedelta(minutes=15)
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'heic', 'heif', 'webp', 'tif',
                       'tiff', 'raw', 'bmp', 'pdf', 'mpeg', 'mpg', 'ogg', 'mp4', 'avi', 'mov'}
+# with open('/Users/errol/gcpKeys/tstreck.json') as jsonFile:
+#     credentials_dict = json.load(jsonFile)
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def upload_via_signed(bucket_name, blob_name, filename, expiration, file_type):
+    bucket = cloudStorage.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    signed_url = blob.generate_signed_url(
+        method='PUT', expiration=expiration, content_type=file_type)
+    requests.put(signed_url, open(filename.filename, 'rb'),
+                 headers={'Content-Type': file_type})
 
 
 @app.route('/')
@@ -45,13 +72,24 @@ def upload_file():
         if file.filename and allowed_file(file.filename):
             longFileName = prepareFileName(secure_filename(file.filename))
             filename = longFileName.split(app.config['UPLOAD_FOLDER'], 1)[1]
+            if app.config['LOCAL_UPLOAD']:
+                if DEBUG:
+                    print(
+                        f'Trying to save file: {secure_filename(file.filename)} as: {longFileName}, {type(longFileName)}')
+                file.save(longFileName)
+                if DEBUG:
+                    print('File uploaded successfully')
             if app.config['GCS_UPLOAD']:
+                if DEBUG:
+                    print(f'GCS: {filename}')
                 fileHash = hashlib.md5(file.read()).hexdigest()
                 file.seek(0)
                 if not cloudStorage.dupExists(fileHash) and not cloudStorage.fileExists(filename):
                     print("Uploading File")
-                    cloudStorage.uploadFile(
-                        file, f"{app.config['UPLOAD_FOLDER']}{filename}", formInfo['name'], formInfo['email'])
+                    # cloudStorage.uploadFile(
+                    # file, f"{app.config['UPLOAD_FOLDER']}{filename}", formInfo['name'], formInfo['email'])
+                    upload_via_signed(bucketName, filename,
+                                      file, EXPIRATION, file.content_type)
                 else:
                     print("Image has already been uploaded")
                 # return 'File Successfully Uploaded to GCS'
@@ -71,16 +109,41 @@ def file_too_large(e):
 def prepareFileName(fileExists, iter=1):
     if not app.config['UPLOAD_FOLDER'] in fileExists:
         fileExists = os.path.join(app.config['UPLOAD_FOLDER'], fileExists)
+    if DEBUG:
+        print("Starting File Check")
+    if app.config['LOCAL_UPLOAD']:
+        if DEBUG:
+            print(os.path.isfile(fileExists))
+        if not os.path.isfile(fileExists):
+            if DEBUG:
+                print(f'return:{fileExists}')
+            return fileExists.lower()
     if app.config['GCS_UPLOAD']:
+        if DEBUG:
+            print(cloudStorage.fileExists(fileExists))
         if not cloudStorage.fileExists(fileExists):
+            if DEBUG:
+                print(f'GCS FileName:{fileExists}')
             return fileExists.lower()
 
+    if DEBUG:
+        print('File exists')
     baseFile = f'{fileExists.rsplit(".", 1)[0].lower()}'
     fileExt = f'.{fileExists.rsplit(".", 1)[1].lower()}'
+    if DEBUG:
+        print(baseFile)
+    if DEBUG:
+        print(f'-{iter}')
     if f'-{iter}' in baseFile:
+        if DEBUG:
+            print("iter found")
         baseFile = f'{baseFile.rsplit("-", 1)[0]}'
         iter = iter + 1
+        if DEBUG:
+            print(f'Base File Name: {baseFile}')
     fileExists = f'{baseFile}-{iter}{fileExt}'
+    if DEBUG:
+        print(f'New Filename: {fileExists}')
     return prepareFileName(fileExists, iter)
 
 
